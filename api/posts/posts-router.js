14a8cb309701cfbf5a0bd010833ea69155cf2c26
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-11-08 08:50:19 -0700

    Create get of posts and get of post by id in posts-routerjs

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,36 @@
 // implement your posts router here
+
+const express = require('express')
+const Data = require('./posts-model')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const fetchedPosts = await Data.find()
+        res.status(200).json(fetchedPosts)
+    } catch(err) {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved '
+        })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const fetchedPost = await Data.findById(id)
+        if (!fetchedPost) {
+            res.status(404).json({
+                message: `The post with the specified ${id} does not exist`
+            })
+        } else {
+            res.status(200).json(fetchedPost)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: 'The post information could not be retrieved'
+        })
+    }
+})
+
+module.exports = router
